d2aa8c4f92a205d0ca704e9c6e7a1b231efc5afc
Sensei <higormuros@gmail.com>
2022-09-18 11:08:15 -0300

    Rotas do site criadas
    Rotas testadas e funcionando

---
+++ app/controllers/HomeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\controllers;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class HomeController{
+    public function index(Request $request, Response $response){
+        $response->getBody()->write("Hello");
+        return $response;
+    }
+
+}
