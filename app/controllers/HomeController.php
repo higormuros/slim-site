581d531b9602546b33496a528ebd7ae396a8357d
Sensei <higormuros@gmail.com>
2022-09-18 11:28:13 -0300

    Comentários adicionados
    Comentários referentes à criação de novas pastas nos projeto

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,5 +1,6 @@
 <?php
-
+//pasta app precisou ser mencionada em composer.json
+//também foi necessário usar o cmd "composer dump-autoload" para a pasta app ser enxergada pelo slim
 namespace app\controllers;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
